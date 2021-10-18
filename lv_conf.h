db175e43bbe0bf2b0a7b4863fe1ad1a2decdba31
hypermasa <phiky@qq.com>
2021-10-18 16:03:32 +0800

    Feature: add unascii for clock activity

--- lv_conf.h
+++ lv_conf.h
@@ -310,7 +310,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_FONT_MONTSERRAT_8     0
 #define LV_FONT_MONTSERRAT_10    0
 #define LV_FONT_MONTSERRAT_12    0
-#define LV_FONT_MONTSERRAT_14    1
+#define LV_FONT_MONTSERRAT_14    0
 #define LV_FONT_MONTSERRAT_16    1
 #define LV_FONT_MONTSERRAT_18    0
 #define LV_FONT_MONTSERRAT_20    0
@@ -337,7 +337,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_FONT_SYST_16          1
 
 /*Pixel perfect monospace fonts*/
-#define LV_FONT_UNSCII_8        0
+#define LV_FONT_UNSCII_8        1
 #define LV_FONT_UNSCII_16       0
 
 /*Optionally declare custom fonts here.
@@ -346,7 +346,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_FONT_CUSTOM_DECLARE
 
 /*Always set a default font*/
-#define LV_FONT_DEFAULT &lv_font_montserrat_14
+#define LV_FONT_DEFAULT &lv_font_montserrat_16
 
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
